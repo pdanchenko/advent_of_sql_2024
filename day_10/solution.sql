6551b56f0485b191734b70f80a69195b004c350e
pdanchenko <pdanchenko@gmail.com>
2024-12-10 13:15:26 +0400

    day10: added one more solution

--- day_10/solution.sql
+++ day_10/solution.sql
@@ -11,6 +11,21 @@ SELECT date FROM drinks WHERE drink_name = 'Eggnog'
 ;
 
 
+-- The most straightforward solution:
+WITH cnt AS (
+SELECT
+    date,
+    SUM(CASE WHEN drink_name = 'Hot Cocoa' THEN quantity ELSE 0 END) AS hot_cocoa,
+    SUM(CASE WHEN drink_name = 'Peppermint Schnapps' THEN quantity ELSE 0 END) AS schnapps,
+    SUM(CASE WHEN drink_name = 'Eggnog' THEN quantity ELSE 0 END) AS eggnog
+FROM drinks
+GROUP BY date
+)
+SELECT date
+FROM cnt
+WHERE hot_cocoa = 38 AND schnapps = 298 AND eggnog = 198
+;
+
 -- Good approach with FILTER functionality I didn't know about
 -- https://github.com/PetitCoinCoin/advent-of-sql-2024/blob/main/day_10.sql
 SELECT
